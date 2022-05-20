0f1b8aa953295f9aa76d47ad83d60ef438cfdc7e
PDevNet <98460053+PDevNet@users.noreply.github.com>
2022-05-20 12:05:07 +0100

    Add files via upload

---
+++ run.py
@@ -0,0 +1,235 @@
+from SpeechCommands.utils import count_parameters
+import torch
+import os
+import numpy as np
+import copy
+#from src.runner.test import test
+import datetime
+import ml_collections
+import yaml
+from SpeechCommands.dataloader import get_dataset
+from SpeechCommands.utils import model_path, EarlyStopping
+import argparse
+
+
+def train_SC(
+    model, dataloader, config, test_loader
+):
+
+    # Training parameters
+    epochs = config.epochs
+    device = config.device
+    # clip = config.clip
+
+    # Save best performing weights
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+    best_loss = 999
+    # iterate over epochs
+    print(model)
+
+    optimizer = torch.optim.Adam(
+        model.parameters(),
+        lr=config.lr,
+        weight_decay=config.weight_decay,
+    )
+    lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(
+        optimizer,
+        gamma=config.gamma)
+    criterion = torch.nn.CrossEntropyLoss()
+    counter = 0
+    # wandb.watch(model, criterion, log="all", log_freq=1)
+    for epoch in range(epochs):
+        print("Epoch {}/{}".format(epoch + 1, epochs))
+        print("-" * 30)
+        # Print current learning rate
+        for param_group in optimizer.param_groups:
+            print("Learning Rate: {}".format(param_group["lr"]))
+        print("-" * 30)
+        # log learning_rate of the epoch
+
+        # Each epoch consist of training and validation
+        for phase in ["train", "validation"]:
+            if phase == "train":
+                model.train()
+            else:
+                model.eval()
+
+            # Accumulate accuracy and loss
+            running_loss = 0
+            running_corrects = 0
+            total = 0
+            # iterate over data
+            for inputs, labels in dataloader[phase]:
+                inputs = inputs.permute(0, 2, 1).to(device)
+                labels = labels.to(device)
+
+                optimizer.zero_grad()
+                train = phase == "train"
+                with torch.set_grad_enabled(train):
+                    # FwrdPhase:
+                    # inputs = torch.dropout(inputs, config.dropout_in, train)
+                    outputs = model(inputs)
+                    loss = criterion(outputs, labels)
+                    # Regularization:
+                    _, preds = torch.max(outputs, 1)
+                    # BwrdPhase:
+                    if phase == "train":
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += (preds == labels).sum().item()
+                total += labels.size(0)
+
+            # statistics of the epoch
+            epoch_loss = running_loss / total
+            epoch_acc = running_corrects / total
+            print("{} Loss: {:.4f} Acc: {:.4f}".format(
+                phase, epoch_loss, epoch_acc))
+            print(datetime.datetime.now())
+
+            # If better validation accuracy, replace best weights and compute the test performance
+            if phase == "validation" and epoch_acc >= best_acc:
+
+                # Updates to the weights will not happen if the accuracy is equal but loss does not diminish
+                if (epoch_acc == best_acc) and (epoch_loss > best_loss):
+                    pass
+                else:
+                    best_acc = epoch_acc
+                    best_loss = epoch_loss
+
+                    best_model_wts = copy.deepcopy(model.state_dict())
+
+                    # Clean CUDA Memory
+                    del inputs, outputs, labels
+                    torch.cuda.empty_cache()
+                    # Perform test and log results
+
+                    test_acc = test_SC(model, test_loader, config)
+
+            if phase == "validation":
+                torch.optim.lr_scheduler.ReduceLROnPlateau(
+                    optimizer, 'min').step(metrics=best_loss)
+                EarlyStopping(patience=30)(val_acc=best_acc)
+        if counter > config.patience:
+            break
+        else:
+            lr_scheduler.step()
+            print()
+
+        lr_scheduler.step()
+        print()
+    # Report best results
+    print("Best Val Acc: {:.4f}".format(best_acc))
+    # Load best model weights
+    model.load_state_dict(best_model_wts)
+    torch.save(model.state_dict(), config.path)
+    # Save the model in the exchangeable ONNX format
+    torch.save(model.state_dict(), config.path)
+
+    # Return model and histories
+    return model
+
+
+def test_SC(model, test_loader, config):
+    # send model to device
+    device = config.device
+
+    model.eval()
+    model.to(device)
+
+    # Summarize results
+    correct = 0
+    total = 0
+
+    with torch.no_grad():
+        # Iterate through data
+        for inputs, labels in test_loader:
+
+            inputs = inputs.permute(0, 2, 1).to(device)
+            labels = labels.to(device)
+
+            outputs = model(inputs)
+            _, predicted = torch.max(outputs.data, 1)
+
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    # Print results
+    test_acc = correct / total
+    print(
+        "Accuracy of the network on the {} test samples: {}".format(
+            total, (100 * test_acc)
+        )
+    )
+    return test_acc
+
+
+def main(config):
+    # print(config)
+    os.environ["CUDA_VISIBLE_DEVICES"] = config.gpu_id
+    print(os.environ["CUDA_VISIBLE_DEVICES"])
+    # Set the seed
+    # torch.manual_seed(config.seed)
+    # np.random.seed(config.seed)
+
+    print(config)
+    if (config.device ==
+            "cuda" and torch.cuda.is_available()):
+        config.update({"device": "cuda:0"}, allow_val_change=True)
+    else:
+        config.update({"device": "cpu"}, allow_val_change=True)
+    torch.cuda.set_per_process_memory_fraction(0.5, 0)
+    from SpeechCommands.models import get_model
+
+    model = get_model(config)
+    num_param = count_parameters(model)
+    print('num_param;', num_param)
+
+    # Define transforms and create dataloaders
+    dataloaders, test_loader = get_dataset(config, num_workers=4)
+
+    # Create model directory and instantiate config.path
+    model_path(config)
+
+    if config.pretrained:
+        # Load model state dict
+        model.module.load_state_dict(torch.load(config.path), strict=False)
+
+    # Train the model
+    if config.train:
+        # Print arguments (Sanity check)
+
+        # Train the model
+        import datetime
+
+        print(datetime.datetime.now())
+        train_SC(model, dataloaders, config, test_loader)
+
+    # Select test function
+    test_acc = test_SC(model, test_loader, config)
+    return test_acc
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', type=str, default='LSTM_DEV',
+                        help='choose from LSTM, DEV, LSTM_DEV, signature')
+
+    args = parser.parse_args()
+    if args.model == 'LSTM_DEV':
+        with open('SpeechCommands/configs/train_lstm_dev.yaml') as file:
+            config = ml_collections.ConfigDict(yaml.safe_load(file))
+    elif args.model == 'LSTM':
+        with open('SpeechCommands/configs/train_lstm.yaml') as file:
+            config = ml_collections.ConfigDict(yaml.safe_load(file))
+    elif args.model == 'DEV':
+        with open('SpeechCommands/configs/train_dev.yaml') as file:
+            config = ml_collections.ConfigDict(yaml.safe_load(file))
+    elif args.model == 'signature':
+        with open('SpeechCommands/configs/train_sig.yaml') as file:
+            config = ml_collections.ConfigDict(yaml.safe_load(file))
+
+    main(config)
---
+++ utils.py
@@ -0,0 +1,185 @@
+import torch
+import os
+import sklearn.model_selection
+import pickle
+import pathlib
+import ml_collections
+import yaml
+
+
+def pad(channel, maxlen):
+    channel = torch.tensor(channel)
+    out = torch.full((maxlen,), channel[-1])
+    out[: channel.size(0)] = channel
+    return out
+
+
+def subsample(X, y, subsample_rate):
+    if subsample_rate != 1:
+        X = X[:, :, ::subsample_rate]
+    return X, y
+
+
+def save_data(dir, **tensors):
+    for tensor_name, tensor_value in tensors.items():
+        torch.save(tensor_value, str(dir / tensor_name) + ".pt")
+
+
+def load_data(dir):
+    tensors = {}
+    for filename in os.listdir(dir):
+        if filename.endswith(".pt"):
+            tensor_name = filename.split(".")[0]
+            tensor_value = torch.load(str(dir / filename))
+            tensors[tensor_name] = tensor_value
+    return tensors
+
+
+def normalise_data(X, y):
+    train_X, _, _ = split_data(X, y)
+    out = []
+    for Xi, train_Xi in zip(X.unbind(dim=-1), train_X.unbind(dim=-1)):
+        train_Xi_nonan = train_Xi.masked_select(~torch.isnan(train_Xi))
+        # compute statistics using only training data.
+        mean = train_Xi_nonan.mean()
+        std = train_Xi_nonan.std()
+        out.append((Xi - mean) / (std + 1e-5))
+    out = torch.stack(out, dim=-1)
+    return out
+
+
+def split_data(tensor, stratify):
+    # 0.7/0.15/0.15 train/val/test split
+    (
+        train_tensor,
+        testval_tensor,
+        train_stratify,
+        testval_stratify,
+    ) = sklearn.model_selection.train_test_split(
+        tensor,
+        stratify,
+        train_size=0.7,
+        random_state=0,
+        shuffle=True,
+        stratify=stratify,
+    )
+
+    val_tensor, test_tensor = sklearn.model_selection.train_test_split(
+        testval_tensor,
+        train_size=0.5,
+        random_state=1,
+        shuffle=True,
+        stratify=testval_stratify,
+    )
+    return train_tensor, val_tensor, test_tensor
+
+
+def add_time(X: torch.tensor) -> torch.tensor:
+    """add time dimension to the input time sereies tensor
+
+    Args:
+        X (torch.tensor): time series tensor with shpe (N,C,T)
+
+    Returns:
+         time added time series tensor with shpe (N,C+1,T)
+    """
+    N, C, T = X.shape
+    time = torch.arange(T).view(1, 1, T).repeat(N, 1, 1)
+    X_tfm = torch.cat((X, time), 1)
+    return X_tfm
+
+
+def load_pickle(dir):
+    with open(dir, "rb") as fp:
+        return pickle.load(fp)
+
+
+def save_pickle(data, dir):
+    with open(dir, "wb") as fp:
+        pickle.dump(data, fp)
+
+
+def model_path(config, root="./SpeechCommands/models"):
+
+    root = pathlib.Path(root)
+    filename = "{}".format(config.dataset)
+
+    # Model-specific keys
+    filename += "_model_{}_param_{}_nhid1_{}_nhid2_{}_mfcc_{}".format(
+        config.model,
+        config.param,
+        config.n_hidden1,
+        config.n_hidden2,
+        config.mfcc
+    )
+
+    # Optimization arguments
+    filename += "_gamma_{}".format(config.gamma)
+    filename += "_lr_{}".format(config.lr)
+
+    # Comment
+    if config.comment != "":
+        filename += "_comment_{}".format(config.comment)
+
+    # Add correct termination
+    filename += ".pt"
+
+    # Check if directory exists and warn the user if the it exists and train is used.
+    os.makedirs(root, exist_ok=True)
+    path = root / filename
+    config.path = str(path)
+
+    if config.train and path.exists():
+        print("WARNING! The model exists in directory and will be overwritten")
+
+
+def count_parameters(model: torch.nn.Module) -> int:
+    """
+
+    Args:
+        model (torch.nn.Module): input models
+    Returns:
+        int: number of trainable parameters in the model
+    """
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+
+class EarlyStopping():
+    """
+    Early stopping to stop the training when the loss does not improve after
+    certain epochs.
+    """
+
+    def __init__(self, patience=5, min_delta=0):
+        """
+        :param patience: how many epochs to wait before stopping when loss is
+               not improving
+        :param min_delta: minimum difference between new loss and old loss for
+               new loss to be considered as an improvement
+        """
+        self.patience = patience
+        self.min_delta = min_delta
+        self.counter = 0
+        self.best_acc = None
+        self.early_stop = False
+
+    def __call__(self, val_acc):
+        if self.best_acc == None:
+            self.best_acc = val_acc
+        elif self.best_acc - val_acc < self.min_delta:
+            self.best_acc = val_acc
+            # reset counter if validation loss improves
+            self.counter = 0
+        elif self.best_acc - val_acc >= self.min_delta:
+            self.counter += 1
+            print(
+                f"INFO: Early stopping counter {self.counter} of {self.patience}")
+            if self.counter >= self.patience:
+                print('INFO: Early stopping')
+                self.early_stop = True
+
+
+def load_config(file_dir: str):
+    with open(file_dir) as file:
+        config = ml_collections.ConfigDict(yaml.safe_load(file))
+    return config
